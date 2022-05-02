e16179b4ec715672aba10b5d47ec357f078be36b
Eric Posthumus <windeddragon@gmail.com>
2022-05-02 02:04:25 -0400

    Add my first layout

---
+++ keyboards/gmmk/pro/ansi/keymaps/my_test/rules.mk
@@ -0,0 +1,6 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_ENCODERS_ENABLE = yes
+# asymmetrical eager/deferred debounce for lower input latency
+# If using tactile/clicky switch edit config.h in gmmk/pro to 8, keep at 5 for linear
+DEBOUNCE_TYPE = asym_eager_defer_pk
