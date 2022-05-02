55c078c7fc8b7412fc9bfdf4a20bb52ad2240696
Eric Posthumus <windeddragon@gmail.com>
2022-05-02 18:03:24 -0400

    Enable LTO to reduce size

--- keyboards/gmmk/pro/keymaps/windeddragon/rules.mk
+++ keyboards/gmmk/pro/keymaps/windeddragon/rules.mk
@@ -4,3 +4,6 @@ VIAL_ENCODERS_ENABLE = yes
 # asymmetrical eager/deferred debounce for lower input latency
 # If using tactile/clicky switch edit config.h in gmmk/pro to 8, keep at 5 for linear
 DEBOUNCE_TYPE = asym_eager_defer_pk
+# Reduce final size
+LTO_ENABLE = yes
+
