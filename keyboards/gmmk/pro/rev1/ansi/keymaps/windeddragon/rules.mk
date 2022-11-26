6f2f4830760fdc308db9ec28ff0a3cd4854f1082
Eric Posthumus <windeddragon@gmail.com>
2022-11-26 02:18:34 -0500

    Move my keymap to rev1

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/windeddragon/rules.mk
@@ -0,0 +1,14 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
+VIAL_ENCODERS_ENABLE = yes
+VIAL_INSECURE = no
+# asymmetrical eager/deferred debounce for lower input latency
+# If using tactile/clicky switch edit config.h in gmmk/pro to 8, keep at 5 for linear
+DEBOUNCE_TYPE = asym_eager_defer_pk
+# Reduce final size
+LTO_ENABLE = yes
+QMK_SETTINGS = no
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
